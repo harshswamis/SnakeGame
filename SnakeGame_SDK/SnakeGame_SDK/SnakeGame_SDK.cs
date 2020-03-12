1808323e7304147ef5d4b9a23bc242f8bdcefa7f
Harshvardhan Swami <HarshvardhanS@Cybertech.Com>
2020-03-12 23:31:36 +0530

    my first commit

---
+++ SnakeGame_SDK/SnakeGame_SDK/SnakeGame_SDK.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Snake;
+using System.Windows.Forms;
+
+namespace SnakeGame_SDK
+{
+    public class SnakeGame_SDK : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public SnakeGame_SDK()
+        {
+            
+        }
+
+        protected override void OnClick()
+        {
+            
+            Window new1 = new Window();
+            new1.ShowDialog();
+            
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
